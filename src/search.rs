3da26fa1c2a1905c00331c9108461f29d5326a4d
Toudonou <hnounagnonoswald@gmail.com>
2025-08-12 18:20:33 +0200

    fix: alpha-beta correction

--- src/search.rs
+++ src/search.rs
@@ -3,108 +3,77 @@ use crate::utils::{Move, PieceColor};
 use crate::{evaluation, moves_generator};
 
 pub fn best_move(position: &Position) -> Move {
-    let moves = moves_generator::generate_legal_moves(position, &position.get_turn());
-    let mut final_move = moves[0].clone();
-    let mut score: f64 = match position.get_turn() {
-        PieceColor::None => 0.0,
-        PieceColor::White => f64::NEG_INFINITY,
-        PieceColor::Black => f64::INFINITY,
-    };
-    let depth: u16 = 3 * 2;
-
+    let mut moves = moves_generator::generate_legal_moves(position, &position.get_turn());
+    let mut best_move = moves[0].clone();
+    let turn = position.get_turn();
+    let mut score = -1_000_000 * turn.clone() as i32;
+    let depth: usize = 5;
     for m in moves {
         let mut temp_position = position.clone();
-        match temp_position.get_turn() {
-            PieceColor::None => {}
+        temp_position.make_move(&m, true);
+        let eval = alpha_beta(&temp_position, depth - 1, -1_000_000, 1_000_000);
+        match turn {
             PieceColor::White => {
-                temp_position.make_move(&m, true);
-                let result = alpha_beta(
-                    &temp_position,
-                    &depth,
-                    &mut f64::NEG_INFINITY,
-                    &mut f64::INFINITY,
-                );
-                if score <= result {
-                    score = result;
-                    final_move = m;
-                    println!(
-                        "best-move {}{}{}{} => {}",
-                        ('a' as u8 + (final_move.source % 8) as u8) as char,
-                        1 + final_move.source / 8,
-                        ('a' as u8 + (final_move.destination % 8) as u8) as char,
-                        1 + final_move.destination / 8,
-                        score
-                    );
+                if eval > score {
+                    score = eval;
+                    best_move = m;
                 }
             }
             PieceColor::Black => {
-                temp_position.make_move(&m, true);
-                let result = alpha_beta(
-                    &temp_position,
-                    &depth,
-                    &mut f64::NEG_INFINITY,
-                    &mut f64::INFINITY,
-                );
-                if score >= result {
-                    score = result;
-                    final_move = m;
-                    println!(
-                        "best-move {}{}{}{} => {}",
-                        ('a' as u8 + (final_move.source % 8) as u8) as char,
-                        1 + final_move.source / 8,
-                        ('a' as u8 + (final_move.destination % 8) as u8) as char,
-                        1 + final_move.destination / 8,
-                        score
-                    );
+                if eval < score {
+                    score = eval;
+                    best_move = m;
                 }
             }
+            _ => {}
         }
     }
-
-    final_move
+    best_move
 }
 
-fn alpha_beta(position: &Position, depth: &u16, alpha: &mut f64, beta: &mut f64) -> f64 {
-    if *depth == 0 {
+fn alpha_beta(position: &Position, depth: usize, mut alpha: i32, mut beta: i32) -> i32 {
+    if depth == 0 {
         return evaluation::evaluate(position);
     }
 
-    let moves = moves_generator::generate_legal_moves(position, &position.get_turn());
-    match position.get_turn() {
-        PieceColor::None => 0.0,
-        PieceColor::White => {
-            // To prioritize checkmate
-            let mut score = if moves.len() == 0 {
-                f64::NEG_INFINITY
+    let mut moves = moves_generator::generate_legal_moves(position, &position.get_turn());
+    if moves.is_empty() {
+        return if position.is_check(&position.get_turn()) {
+            if position.get_turn() == PieceColor::White {
+                -1_000_000
             } else {
-                -500_000.0
-            };
+                1_000_000
+            }
+        } else {
+            0 // stalemate
+        };
+    }
+
+    match position.get_turn() {
+        PieceColor::None => 0,
+        PieceColor::White => {
+            let mut score = -100_000;
             for mov in moves {
                 let mut temp_position = position.clone();
                 temp_position.make_move(&mov, true);
-                score = score.max(alpha_beta(&temp_position, &(depth - 1), alpha, beta));
-                if score >= *beta {
+                score = score.max(alpha_beta(&temp_position, depth - 1, alpha, beta));
+                alpha = alpha.max(score);
+                if beta <= alpha {
                     break;
                 }
-                *alpha = alpha.max(score);
             }
             score
         }
         PieceColor::Black => {
-            // To prioritize checkmate
-            let mut score = if moves.len() == 0 {
-                f64::INFINITY
-            } else {
-                500_000.0
-            };
+            let mut score = 100_000;
             for mov in moves {
                 let mut temp_position = position.clone();
                 temp_position.make_move(&mov, true);
-                score = score.min(alpha_beta(&temp_position, &(depth - 1), alpha, beta));
-                if score <= *alpha {
+                score = score.min(alpha_beta(&temp_position, depth - 1, alpha, beta));
+                beta = beta.min(score);
+                if beta <= alpha {
                     break;
                 }
-                *beta = beta.min(score);
             }
             score
         }
